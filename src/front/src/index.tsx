d7cfe3071518d20907eb1b7af39f7549bad2a756
Casaubon <netzach.yoshida@gmail.com>
2018-12-08 21:09:01 +0900

    add index.tsx

--- src/front/src/index.tsx
+++ src/front/src/index.tsx
@@ -1,25 +1,38 @@
 import { Button } from '@blueprintjs/core'
+import axios from 'axios'
 import React from "react"
 import ReactDOM from "react-dom"
 
-class Counter extends React.Component<{}, { value: number }> {
-  state = {
-    value: 0
+interface TasksState {
+  tasks?: string
+}
+
+class Counter extends React.Component<{}, TasksState> {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      tasks: ''
+    }
+    this.getData = this.getData.bind(this)
   }
+
+  getData() {
+    axios
+      .get('http://127.0.0.1:3000/')
+      .then(results => {
+        const data = results.data
+        console.log(data)
+        this.setState({
+          tasks: '' + data
+        })
+      })
+  }
+
   render() {
     return(
       <div>
-        <Button icon='plus'
-          onClick={() => {
-            this.setState({ value: this.state.value + 1 })
-          }}
-        />
-        <Button icon='minus'
-          onClick={() => {
-            this.setState({ value: this.state.value - 1 })
-          }}
-        />
-        <div>{this.state.value}</div>
+        <Button onClick={this.getData}>getData</Button>
+        <div>{this.state.tasks}</div>
       </div>
     )
   }
